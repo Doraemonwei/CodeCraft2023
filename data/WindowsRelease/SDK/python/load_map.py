6e5a71481da2e7ae72db0f4aa4606b02b6a08900
lanpangzi <1470028580@qq.com>
2023-03-10 16:27:02 +0800

    readme方案添加 地图加载完善

--- data/WindowsRelease/SDK/python/load_map.py
+++ data/WindowsRelease/SDK/python/load_map.py
@@ -2,7 +2,7 @@
 # @Time : 2023/3/10 12:48
 # @Author : Lanpangzi
 # @File : load_map.py
-
+import sys
 
 bench_ids = [str(i) for i in range(1, 10)]
 
@@ -13,12 +13,18 @@ def read_map_util_ok():
     benches = []
     row = 1
     m_input = input()
+    robots_id = 0
+    bench_id = 0
     while m_input != "OK":
         for ind, j in enumerate(m_input):
             if j == '.':
                 continue
             elif j in bench_ids:
-                # [编号, [x,y]] 坐标是中心坐标
-                benches.append([int(j), [0.25 + (ind+1) * 0.5, 50 - (0.25 + 0.5 * row)]])
+                # [唯一id,种类编号,[x,y]] 坐标是中心坐标
+                benches.append([bench_id, int(j), [0.25 + (ind + 1) * 0.5, 50 - (0.25 + 0.5 * row)]])
+                bench_id += 1
             else:
-                robots
+                # [唯一id, [x,y]] 坐标是中心坐标
+                robots.append([robots_id, [0.25 + (ind + 1) * 0.5, 50 - (0.25 + 0.5 * row)]])
+                robots_id += 1
+    return robots, benches
--- data/WindowsRelease/SDK/python/main.py
+++ data/WindowsRelease/SDK/python/main.py
@@ -1,10 +1,32 @@
 #!/bin/bash
 import sys
+from load_map import read_map_util_ok
 
 
-def read_util_ok():
-    while input() != "OK":
-        pass
+class Bench123:
+    # 类型1、2、3的工作台
+    def __init__(self, id_, pos, type_, product):
+        self.id = id_  # 这个工作台的唯一id，标记用
+        self.type = type_  # 这个bench是123中的哪一种
+        self.product = product  # 生产出来的产品类型
+        self.pos = pos
+        self.run_period = 50  # 这个工作台的生产周期，其实123都是50
+        self.left_time = 50  # 初始时就开始制造
+        self.has_product = False  # 初始时没有产品
+
+    # 拿走了已经生产好了的1号产品，改变剩余时间和是否含有产品的标记
+    def take_product(self):
+        self.left_time = 50
+        self.has_product = False
+
+    # 每一帧都需要对这个玩意进行更新
+    def update(self):
+        # 如果还需要时间生产，更新后剩余时间-1
+        if self.left_time >= 1:
+            self.left_time -= 1
+        # 如果更新后发现生产完毕，跟新标记变量
+        if self.left_time == 0:
+            self.has_product = True
 
 
 def finish():
@@ -13,8 +35,14 @@ def finish():
 
 
 if __name__ == '__main__':
-    read_util_ok()
+
+    # 读取机器人以及工作台的位置信息
+    robots, benches = read_map_util_ok()
+    # 每一次与判题器交互后都要输出ok并flush
     finish()
+
+    # 根据初始的map信息初始化所有的工作台和机器人信息
+
     while True:
         line = sys.stdin.readline()
         if not line:
@@ -22,7 +50,6 @@ if __name__ == '__main__':
         parts = line.split(' ')
         frame_id = int(parts[0])
         read_util_ok()
-
         sys.stdout.write('%d\n' % frame_id)
         line_speed, angle_speed = 3, 1.5
         for robot_id in range(4):
